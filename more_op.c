d7211aee5b971a7335b037965dfe8e163851c478
Andrew Eshak Wreford <46035160+wrefinity@users.noreply.github.com>
2022-08-11 00:31:29 +0100

    Update more_op.c

--- more_op.c
+++ more_op.c
@@ -1,37 +1,14 @@
 #include "shell.h"
 
 /**
- *_atoi - converts a string to an integer
- *@s: the string to convert
- *Return: 0 if no numbers in string, converted number otherwise
+ * interactive - returns true if shell is interactive mode
+ * @info: struct address
+ *
+ * Return: 1 if interactive mode, 0 otherwise
  */
-
-int _atoi(char *s)
+int interactive(info_t *info)
 {
-	int i, sign = 1, flag = 0, ret;
-	unsigned int result = 0;
-
-	for (i = 0;  s[i] && flag != 2; i++)
-	{
-		if (s[i] == '-')
-			sign *= -1;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			flag = 1;
-			result *= 10;
-			result += (s[i] - '0');
-		}
-		else if (flag == 1)
-			flag = 2;
-	}
-
-	if (sign == -1)
-		ret = -result;
-	else
-		ret = result;
-
-	return (ret);
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
@@ -63,13 +40,35 @@ int _isalpha(int c)
 }
 
 /**
- * is_interactive - returns true if shell is interactive mode
- * @info: struct address
- *
- * Return: 1 if interactive mode, 0 otherwise
+ *_atoi - converts a string to an integer
+ *@s: the string to be converted
+ *Return: 0 if no numbers in string, converted number otherwise
  */
-int is_interactive(info_t *info)
-{
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
-}
 
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
+}
