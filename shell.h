5ee7ee7ac10da2869bd41cc0dc89605f963670a0
Andrew Eshak Wreford <46035160+wrefinity@users.noreply.github.com>
2022-08-10 23:48:29 +0100

    update: prototype adds

--- shell.h
+++ shell.h
@@ -1,9 +1,6 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -11,29 +8,31 @@
 #include <limits.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-/* 1 if using system getline() */
-#define USE_GETLINE 0
-#define USE_STRTOK 0
 
-#define HIST_FILE	".simple_shell_history"
-#define HIST_MAX	4096
-
-/* for convert_number() */
-#define CONVERT_LOWERCASE	1
-#define CONVERT_UNSIGNED	2
-
-/* for read/write buffers */
-#define READ_BUF_SIZE 1024
-#define WRITE_BUF_SIZE 1024
-#define BUF_FLUSH -1
-
-/* for command chaining */
+/* for command parameter */
 #define CMD_NORM	0
 #define CMD_OR		1
 #define CMD_AND		2
 #define CMD_CHAIN	3
 
+/* buffer parameters */
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+#define CONVERT_LOWERCASE	1
+#define CONVERT_UNSIGNED	2
+
+
+#define USE_GETLINE 0
+#define USE_STRTOK 0
+
+#define HIST_FILE	".simple_shell_history"
+#define HIST_MAX	4096
 
 extern char **environ;
 
@@ -52,15 +51,10 @@ typedef struct liststr
 } list_t;
 
 /**
- *struct passinfo - contains pseudo-arguements to pass into a function,
- *					allowing uniform prototype for function pointer struct
- *@arg: a string generated from getline containing arguements
+ *struct passinfo -  pointer struct parameter
+ *@arg: command line arguments
  *@argv: an array of strings generated from arg
  *@path: a string path for the current command
- *@argc: the argument count
- *@line_count: the error count
- *@err_num: the error code for exit()s
- *@linecount_flag: if on count this line of input
  *@fname: the program filename
  *@env: linked list local copy of environ
  *@environ: custom modified copy of environ from LL env
@@ -72,6 +66,10 @@ typedef struct liststr
  *@cmd_buf_type: CMD_type ||, &&, ;
  *@readfd: the fd from which to read line input
  *@histcount: the history line number count
+ *@argc: the argument count
+ *@line_count: the error count
+ *@err_num: the error code for exit()s
+ *@linecount_flag: if on count this line of input
  */
 typedef struct passinfo
 {
@@ -111,28 +109,23 @@ typedef struct builtin
 	int (*func)(info_t *);
 } builtin_table;
 
-
-/* shell.c */
-int loop_sh(info_t *, char **);
+/* path command  */
+int is_cmd(info_t *, char *);
+char *dup_chars(char *, int, int);
+char *find_path(info_t *, char *, char *);
+int loophsh(char **);
+int hsh(info_t *, char **);
 int find_builtin(info_t *);
 void find_cmd(info_t *);
 void fork_cmd(info_t *);
 
-/* path finder functions */
-int is_cmd(info_t *, char *);
-char *dup_chars(char *, int, int);
-char *find_path(info_t *, char *, char *);
-
-/* loophsh.c */
-int loophsh(char **);
-
-/* error_print.c */
+/* string error printer */
 void _eputs(char *);
 int _eputchar(char);
-int _putfd(char c, int dsc);
-int _putsdsc(char *str, int dsc);
+int _putfd(char c, int fd);
+int _putsfd(char *str, int fd);
 
-/* string helper functions */
+/* built string function*/
 int _strlen(char *);
 int _strcmp(char *, char *);
 char *starts_with(const char *, const char *);
@@ -144,78 +137,73 @@ int _putchar(char);
 char *_strncpy(char *, char *, int);
 char *_strncat(char *, char *, int);
 char *_strchr(char *, char);
-
-/* string_word.c */
-char **str_word(char *, char *);
-char **str__word(char *, char);
+char **strtow(char *, char *);
+char **strtow2(char *, char);
 
 /* memory_functions */
 char *_memset(char *, char, unsigned int);
-void str_free(char **);
+void ffree(char **);
 void *_realloc(void *, unsigned int, unsigned int);
 int bfree(void **);
 
-/* more_op.c */
-int is_interactive(info_t *);
+/* more string and number func */
+int interactive(info_t *);
 int is_delim(char, char *);
 int _isalpha(int);
 int _atoi(char *);
-
 int _erratoi(char *);
 void print_error(info_t *, char *);
 int print_d(int, int);
 char *convert_number(long int, int, int);
 void remove_comments(char *);
 
-/* advance function */
-int _shellexit(info_t *);
-int _cdir(info_t *);
-int _cdhelp(info_t *);
-int _history(info_t *);
-int _alias(info_t *);
+/* advance commands */
+int _myexit(info_t *);
+int _mycd(info_t *);
+int _myhelp(info_t *);
+int _myhistory(info_t *);
+int _myalias(info_t *);
 
-/* getline.c module */
+/* getline function */
 ssize_t get_input(info_t *);
 int _getline(info_t *, char **, size_t *);
 void sigintHandler(int);
 
-/* info.c module */
+/* add on functions for infor struct */
 void clear_info(info_t *);
 void set_info(info_t *, char **);
 void free_info(info_t *, int);
 
-/* function for the enviroment  */
-int _environ(info_t *);
-int _setenviron(info_t *);
-int _unsetenviron(info_t *);
-char *_getenviron(info_t *, const char *);
+/* dealing with enviromental varaible */
+char *_getenv(info_t *, const char *);
+int _myenv(info_t *);
+int _mysetenv(info_t *);
+int _myunsetenv(info_t *);
 int populate_env_list(info_t *);
 char **get_environ(info_t *);
 int _unsetenv(info_t *, char *);
 int _setenv(info_t *, char *, char *);
 
-/* file_io_functions.c */
+/* history functions */
 char *get_history_file(info_t *info);
 int write_history(info_t *info);
 int read_history(info_t *info);
 int build_history_list(info_t *info, char *buf, int linecount);
 int renumber_history(info_t *info);
 
-/* liststr.c module */
+/* list functionalities */
 list_t *add_node(list_t **, const char *, int);
 list_t *add_node_end(list_t **, const char *, int);
 size_t print_list_str(const list_t *);
 int delete_node_at_index(list_t **, unsigned int);
 void free_list(list_t **);
-
-/* liststr2.c module */
 size_t list_len(const list_t *);
 char **list_to_strings(list_t *);
 size_t print_list(const list_t *);
 list_t *node_starts_with(list_t *, char *, char);
 ssize_t get_node_index(list_t *, list_t *);
 
-/* chainning operations */
+/* chain commands  */
 int is_chain(info_t *, char *, size_t *);
 void check_chain(info_t *, char *, size_t *, size_t, size_t);
 int replace_alias(info_t *);
