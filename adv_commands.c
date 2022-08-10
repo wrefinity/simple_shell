7ca43a3e1cc327331c947939472aeb3ed7bb8fa0
Andrew Eshak Wreford <46035160+wrefinity@users.noreply.github.com>
2022-08-11 00:38:30 +0100

    Update adv_commands.c

--- adv_commands.c
+++ adv_commands.c
@@ -1,15 +1,13 @@
 #include "shell.h"
 
-
-
 /**
- * _history - displays the history list, one command by line, preceded
+ * _myhistory - displays the history list, one command by line, preceded
  *              with line numbers, starting at 0.
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
  *  Return: Always 0
  */
-int _history(info_t *info)
+int _myhistory(info_t *info)
 {
 	print_list(info->history);
 	return (0);
@@ -83,12 +81,12 @@ int print_alias(list_t *node)
 }
 
 /**
- * _alias - mimics the alias builtin (man alias)
+ * _myalias - mimics the alias builtin (man alias)
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  *  Return: Always 0
  */
-int _alias(info_t *info)
+int _myalias(info_t *info)
 {
 	int i = 0;
 	char *p = NULL;
