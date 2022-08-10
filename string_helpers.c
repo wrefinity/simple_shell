e71833f1a366e98b225663d11d40597b427195b8
Andrew Eshak Wreford <46035160+wrefinity@users.noreply.github.com>
2022-08-11 00:34:44 +0100

    Update string_helpers.c

--- string_helpers.c
+++ string_helpers.c
@@ -2,42 +2,42 @@
 
 /**
  * _strlen - returns the length of a string
- * @str: the string to check
+ * @s: the string whose length to check
  *
- * Return: length of string
+ * Return: integer length of string
  */
-int _strlen(char *str)
+int _strlen(char *s)
 {
 	int i = 0;
 
-	if (!str)
+	if (!s)
 		return (0);
 
-	while (*str++)
+	while (*s++)
 		i++;
 	return (i);
 }
 
 /**
- * _strcmp - compare two strings.
- * @str1: the first string
- * @str2: the second string
+ * _strcmp - performs lexicogarphic comparison of two strangs.
+ * @s1: the first strang
+ * @s2: the second strang
  *
- * Return: negative if string is not same else zero
+ * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
  */
-int _strcmp(char *str1, char *str2)
+int _strcmp(char *s1, char *s2)
 {
-	while (*str1 && *str2)
+	while (*s1 && *s2)
 	{
-		if (*str1 != *str2)
-			return (*str1 - *str2);
-		str1++;
-		str2++;
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
 	}
-	if (*str1 == *str2)
+	if (*s1 == *s2)
 		return (0);
 	else
-		return (*str1 < *str2 ? -1 : 1);
+		return (*s1 < *s2 ? -1 : 1);
 }
 
 /**
@@ -64,12 +64,12 @@ char *starts_with(const char *haystack, const char *needle)
  */
 char *_strcat(char *dest, char *src)
 {
-	char *ptr = dest;
+	char *ret = dest;
 
 	while (*dest)
 		dest++;
 	while (*src)
 		*dest++ = *src++;
 	*dest = *src;
-	return (ptr);
+	return (ret);
 }
